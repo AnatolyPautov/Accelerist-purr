2695c7b8489fdf079a0732e7f17d76a495967a54
Anatoly Pautov <anatoliy.pautov@purrweb.com>
2021-11-23 12:03:20 +0600

    fix position spinner in search page

--- src/screens/Main/SearchScreen/SearchScreen.tsx
+++ src/screens/Main/SearchScreen/SearchScreen.tsx
@@ -37,7 +37,7 @@ const SearchScreen: React.FC<Props> = ({ page }) => {
 
 const Container = styled.div`
   padding: 32px 0;
-  height: 100%;
+  height: 100vh;
   @media (max-width: 525px) {
     padding: 20px 0;
   }
